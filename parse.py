95259e92fb79466cb2fe07bce69a06ad7c51e10f
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-12-13 16:43:16 -0600

    Parse cvs to dictionary

---
+++ parse.py
@@ -0,0 +1,17 @@
+import csv
+import json
+
+with open ("EjemploCSV.csv", 'r') as f:
+    reader = csv.reader(f)
+    data = {}
+    counter = 0
+    for index in reader:
+        if counter == 0:
+            for row in index:
+                data[row] = ''
+            counter += 1
+        else:
+            for i in range(len(index)):
+                data[list(data.keys())[i]] = index[i]
+
+print(data)
